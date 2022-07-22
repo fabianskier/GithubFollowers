550856cfeb28a9162b19ddc833f9dc2346a4c056
Oscar Cristaldo <fabianskier@icloud.com>
2022-07-22 18:32:55 -0400

    CO-0004: Add keyboard dismiss function and pass data
    - dismissKeyboardTapGesture function
    - FollowerListViewController
    - String extension with regex
    - Set navigation bar tint color inside SceneDelegate
    
    Signed-off-by: Oscar Cristaldo <fabianskier@icloud.com>

---
+++ GithubFollowers/FollowerListViewController.swift
@@ -0,0 +1,22 @@
+//
+//  FollowerListViewController.swift
+//  GithubFollowers
+//
+//  Created by Oscar Cristaldo on 2022-07-22.
+//
+
+import UIKit
+
+class FollowerListViewController: UIViewController {
+    
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+
+}
