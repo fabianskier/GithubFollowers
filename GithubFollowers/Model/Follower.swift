bbe7318ca3d4cb8315eff5b293e1af5a675dda0d
Oscar Cristaldo <fabianskier@icloud.com>
2022-07-23 22:46:06 -0400

    CO-0007: Add model and fix bug
    - Follower model
    - User model
    - Fix navigation bar swipe transition bug
    
    Signed-off-by: Oscar Cristaldo <fabianskier@icloud.com>

---
+++ GithubFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GithubFollowers
+//
+//  Created by Oscar Cristaldo on 2022-07-23.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
