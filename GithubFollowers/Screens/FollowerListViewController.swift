7022c852c002c9fa11c37fe99c807f48d16d9915
Oscar Cristaldo <fabianskier@icloud.com>
2022-07-26 23:37:03 -0400

    CO-0018: Add loading screen
    Signed-off-by: Oscar Cristaldo <fabianskier@icloud.com>

--- GithubFollowers/Screens/FollowerListViewController.swift
+++ GithubFollowers/Screens/FollowerListViewController.swift
@@ -47,10 +47,10 @@ class FollowerListViewController: UIViewController {
     }
     
     func getFollowers(username: String, page: Int, per_page: Int) {
+        showLoadingView()
         NetworkManager.shared.getFollowers(for: username, page: page, per_page: per_page) { [weak self] result in
             guard let self = self else { return }
-            
-            
+            self.dismissLoadingView()
             
             switch result {
             case .success(let followers):
