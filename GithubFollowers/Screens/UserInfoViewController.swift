fc4111e4a883b5fc268eb2783fb95118b2f535e6
Oscar Cristaldo <fabianskier@icloud.com>
2022-07-28 19:47:06 -0400

    CO-0022: Add network call to get user info
    Signed-off-by: Oscar Cristaldo <fabianskier@icloud.com>

--- GithubFollowers/Screens/UserInfoViewController.swift
+++ GithubFollowers/Screens/UserInfoViewController.swift
@@ -16,6 +16,17 @@ class UserInfoViewController: UIViewController {
         view.backgroundColor = .systemBackground
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissViewController))
         navigationItem.rightBarButtonItem = doneButton
+        
+        NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let user):
+                print(user)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, button: "Ok")
+            }
+        }
     }
 
     @objc func dismissViewController() {
