8c20be1f8298eb4c250e83df847dc999d820926c
Oscar Cristaldo <fabianskier@icloud.com>
2022-08-02 22:47:55 -0400

    CO-0030: Add persistence manager
    Signed-off-by: Oscar Cristaldo <fabianskier@icloud.com>

--- GithubFollowers/Screens/FollowerListViewController.swift
+++ GithubFollowers/Screens/FollowerListViewController.swift
@@ -39,6 +39,9 @@ class FollowerListViewController: UIViewController {
     func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
+        navigationItem.rightBarButtonItem = addButton
     }
     
     func configureCollectionView() {
@@ -94,6 +97,10 @@ class FollowerListViewController: UIViewController {
         snapshot.appendSections([.main])
         snapshot.appendItems(followers)
         DispatchQueue.main.async { self.dataSource.apply(snapshot, animatingDifferences: true) }
+    }
+    
+    @objc func addButtonTapped() {
+        
     }
 }
 
