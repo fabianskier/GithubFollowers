0095e1ed44b55fb4aa295f78b9867a420f612dfc
Oscar Cristaldo <fabianskier@icloud.com>
2022-08-03 14:12:14 -0400

    CO-0031: Add and retrieve favorite followers
    Signed-off-by: Oscar Cristaldo <fabianskier@icloud.com>

--- GithubFollowers/Screens/FavoritesListViewController.swift
+++ GithubFollowers/Screens/FavoritesListViewController.swift
@@ -13,6 +13,15 @@ class FavoritesListViewController: UIViewController {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBlue
+        
+        PersistenceManager.retrieveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                print(favorites)
+            case .failure(let error):
+                break
+            }
+        }
     }
 
 
--- GithubFollowers/Screens/FollowerListViewController.swift
+++ GithubFollowers/Screens/FollowerListViewController.swift
@@ -104,7 +104,30 @@ class FollowerListViewController: UIViewController {
     }
     
     @objc func addButtonTapped() {
+        showLoadingView()
         
+        NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
+            guard let self = self else { return }
+            self.dismissLoadingView()
+            
+            switch result {
+            case .success(let user):
+                let favorite = Follower(login: user.login, avatarUrl: user.avatarUrl)
+                
+                PersistenceManager.updateWith(favorite: favorite, actionType: .add) { [weak self] error in
+                    guard let self = self else { return }
+                    guard let error = error else {
+                        self.presentGFAlertOnMainThread(title: "Success!", message: "You have successfully favorited this user.", button: "Ok")
+                        return
+                    }
+                    
+                    self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, button: "Ok")
+
+                }
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, button: "Ok")
+            }
+        }
     }
 }
 
